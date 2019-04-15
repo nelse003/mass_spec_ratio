5f2c60e70a6830a44a82a1ae9196fe3e832e05ac
nelse003 <nelse003@gmail.com>
2019-04-15 22:31:44 +0100

    Add summary information for running exhaustive search

--- exhaustive_NUDT7_cov.py
+++ exhaustive_NUDT7_cov.py
@@ -11,13 +11,36 @@ ccp4_path = "/dls/science/groups/i04-1/software/pandda_0.2.12/ccp4/" \
 
 parse_xchemdb_script_dir = "/dls/science/groups/i04-1/elliot-dev/parse_xchemdb"
 
-#TODO Loop over output files in copy_atoms folder
+copy_atoms_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                 "NUDT7A_mass_spec_refinements/copy_atoms"
 
-for xtal in :
+out_script_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                 "NUDT7A_mass_spec_refinements/copy_atoms/exhaustive_scripts"
+
+out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+                 "NUDT7A_mass_spec_refinements/copy_atoms/exhaustive"
+
+xtal_prefix = "NUDT7A-x"
+
+if not os.path.isdir(out_script_dir):
+    os.makedirs(out_script_dir)
+
+if not os.path.isdir(out_dir):
+    os.makedirs(out_dir)
+
+xtal_folders = [f.name
+                for f in os.scandir(copy_atoms_dir)
+                if f.is_dir()
+                and xtal_prefix in f.name]
+
+for xtal in xtal_folders:
+
+    pdb = os.path.join(copy_atoms_dir, xtal, "refine.pdb")
+    mtz = os.path.join(copy_atoms_dir, xtal, "refine.mtz")
 
     write_exhaustive_csh(pdb=pdb,
                      mtz=mtz,
-                     refinement_script_dir=refinement_script_dir,
+                     refinement_script_dir=out_script_dir,
                      out_dir=out_dir,
                      crystal=xtal,
                      script_dir=parse_xchemdb_script_dir,
---
+++ summarise_exhaustive.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import pandas as pd
+
+sys.path.append("/dls/science/groups/i04-1/elliot-dev/Work/exhaustive_search")
+from exhaustive.exhaustive.utils.utils import u_iso_to_b_fac, get_minimum_fofc
+
+out_dir = "/dls/science/groups/i04-1/elliot-dev/Work/" \
+          "NUDT7A_mass_spec_refinements/copy_atoms/exhaustive"
+
+xtal_prefix = "NUDT7A-x"
+
+xtal_folders = [f.name
+                for f in os.scandir(out_dir)
+                if f.is_dir()
+                and xtal_prefix in f.name]
+
+summary = {}
+
+for xtal in xtal_folders:
+
+    csv = os.path.join(out_dir, xtal, "exhaustive_search.csv")
+
+    occ, u_iso, fo_fc = get_minimum_fofc(csv)
+    summary[xtal] = (occ, u_iso_to_b_fac(u_iso), fo_fc)
+
+df = pd.DataFrame.from_dict(summary,
+                            orient='index',
+                            columns=["occupancy",
+                                     "b_factor",
+                                     "fo_fc"])
+df.index.name = "crystal"
+
+out_csv = os.path.join(out_dir,"exhaustive_minima.csv")
+df.to_csv(out_csv)
